648bda03b53e7b45ad8aafcd59d75b24d439d109
nkambuleat-ux <nkambuleat@gmail.com>
2025-08-30 14:43:15 +0200

    Add files via upload

---
+++ Diabetes/UI/Single_Prediction.py
@@ -0,0 +1,45 @@
+
+import streamlit as st
+import pickle #Helps load the pickle file created during model development
+import numpy as np
+#import os
+
+st.title("Welcome to Diabetes Test")
+st.header("Please fill in your details")
+
+#Features used to train the model
+#use number_input("Instruction", min, max, default value) for numerical values
+#use selectbox for categorical values
+preg=st.number_input("Enter Preganancies",0,10,5)
+glucose=st.number_input("Enter glucose level",0,200)
+blood_pressure=st.number_input("Enter blood pressure",0,200)
+skin=st.number_input("Enter skin thickness",0,200)
+insulin = st.number_input("Enter insulin level",0,200)
+bmi = st.number_input("Enter bmi",0,200)
+pedigreefn = st.number_input("Enter DiabetesPedigreeFunction",0,200)
+age = st.slider('Select your age:', 0, 100, 25)
+#all user inputs are stored in the respective variables(python variables)
+
+#create a button to predict output
+predict_clicked=st.button("Get the prediction")
+
+if predict_clicked==True:
+    model=pickle.load(open("C:/Users/nkamb/VS code/Diabetes/Diabetes_Model_Development/lr.pkl", 'rb'))
+
+    #load the test data into numpy array
+    data=[np.array([preg,glucose,blood_pressure,skin,insulin,bmi,pedigreefn,age])]
+
+    #call the model to predict the price
+    result=model.predict(data)
+    if (result==1):
+        result_string = "Diabetic"
+        st.error("The outcome for your diabetes test is "+result_string)
+    else:
+        result_string = "Non-Diabetic"
+        st.success("The outcome for your diabetes test is "+result_string)
+
+    #display the predicted price on the webpage
+    
+
+
+
